bd915fb960ae74b3b9840c26f0165b7fed40241d
luat <luatvd95@gmail.com>
2019-10-19 11:00:30 +0700

    seeder teacher

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Teacher extends Model
 {
-    //
+    protected $table = 'teachers';
+    protected $guarded = ['id'];
+    protected $casts = [
+        'grade' => 'json',
+    ];
 }
