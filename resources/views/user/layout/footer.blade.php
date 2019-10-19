3437642003a42a9612885efe36b6a81715a181dc
Danh Lợi <danhloimta@gmail.com>
2019-10-20 01:00:04 +0700

    edit header

--- resources/views/user/layout/footer.blade.php
+++ resources/views/user/layout/footer.blade.php
@@ -75,23 +75,6 @@
           </div>
         </div>
       </div>
-
-      <div class="row copyright_row">
-        <div class="col">
-          <div class="copyright d-flex flex-lg-row flex-column align-items-center justify-content-start">
-            <div class="cr_text"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
-            <div class="ml-lg-auto cr_links">
-              <ul class="cr_list">
-                <li><a href="#">Copyright notification</a></li>
-                <li><a href="#">Terms of Use</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </footer>
 </div>
