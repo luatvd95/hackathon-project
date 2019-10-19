9beed4fc44f12243f944d46e70d0ac2a9f25abf6
phanlyhuynh <lyhuynh0806@gmail.com>
2019-10-19 13:11:49 +0700

    Add layout first time question page

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::group(['namespace' => 'User'], function () {
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
 	Route::get('/', 'AdminController@index');
 });
+
+Route::get('suggestions', function () {
+	return view('user.firsttime');
+});
